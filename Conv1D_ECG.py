ec4eba89cac639492d1d7c2f753e70d522c5e52d
Boris <pakillaboris@gmail.com>
2017-06-12 17:51:59 +0700

    84.2 % accuracy

--- Conv1D_ECG.py
+++ Conv1D_ECG.py
@@ -31,13 +31,27 @@ def change(x):  #From boolean arrays to decimal arrays
 mypath = 'training2017/' #Training directory
 onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and f[0] == 'A')]
 bats = [f for f in onlyfiles if f[7] == 'm']
-check = 4500
+check = 2000
 mats = [f for f in bats if (np.shape(sio.loadmat(mypath + f)['val'])[1] >= check)]
 size = len(mats)
 print('Total training size is ', size)
-X = np.zeros((size, check))
+big = 10000
+X = np.zeros((size, big))
+# for i in range(size):
+    # X[i, :] = sio.loadmat(mypath + mats[i])['val'][0, :check]
 for i in range(size):
-    X[i, :] = sio.loadmat(mypath + mats[i])['val'][0, :check]
+    dummy = sio.loadmat(mypath + mats[i])['val'][0, :]
+    if (big - len(dummy)) <= 0:
+        X[i, :] = dummy[0:big]
+    else:
+        b = dummy[0:(big - len(dummy))]
+        goal = np.hstack((dummy, b))
+        while len(goal) != big:
+            b = dummy[0:(big - len(goal))]
+            goal = np.hstack((goal, b))
+        X[i, :] = goal
+	
+print(np.shape(X))	
 
 target_train = np.zeros((size, 1))
 Train_data = pd.read_csv(mypath + 'REFERENCE.csv', sep=',', header=None, names=None)
@@ -60,39 +74,65 @@ for i in range(size):
 X = (X - X.mean())/(X.std()) #Some normalization here
 X = np.expand_dims(X, axis=2) #For Keras's data input size
 
+
+values = [i for i in range(size)]
+permutations = np.random.permutation(values)
+X = X[permutations, :]
+Label_set = Label_set[permutations, :]
+
 train = 0.85 #Size of training set in percentage
 X_train = X[:int(train * size), :]
 Y_train = Label_set[:int(train * size), :]
 X_val = X[int(train * size):, :]
 Y_val = Label_set[int(train * size):, :]
 
+# def train_and_evaluate__model(model, X_train, Y_train, X_val, Y_val, i):
+
+# def create_model():
 model = Sequential()
-model.add(Conv1D(128, 55, activation='relu', input_shape=(check, 1)))
-model.add(Conv1D(128, 35, activation='relu'))
+model.add(Conv1D(128, 50, activation='relu', input_shape=(big, 1)))
 model.add(MaxPooling1D(5))
 model.add(Dropout(0.5))
 model.add(Conv1D(128, 25, activation='relu'))
-model.add(Conv1D(128, 15, activation='relu'))
 model.add(MaxPooling1D(5))
 model.add(Dropout(0.5))
 model.add(Conv1D(128, 10, activation='relu'))
-model.add(Conv1D(128, 5, activation='relu'))
 model.add(MaxPooling1D(5))
 model.add(Dropout(0.5))
 model.add(Conv1D(128, 5, activation='relu'))
-model.add(Conv1D(128, 3, activation='relu'))
 model.add(GlobalAveragePooling1D())
-# model.add(Dropout(0.5))
-model.add(Dense(96, kernel_initializer='normal', activation='relu'))
+model.add(Dense(128, kernel_initializer='normal', activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(128, kernel_initializer='normal', activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, kernel_initializer='normal', activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(number_of_classes, kernel_initializer='normal', activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 checkpointer = ModelCheckpoint(filepath='Conv_models/Best_model.h5', monitor='val_acc', verbose=1, save_best_only=True)
-hist = model.fit(X_train, Y_train, validation_data=(X_val, Y_val), batch_size=350, epochs=500, verbose=2, shuffle=True, callbacks=[checkpointer])
+hist = model.fit(X_train, Y_train, validation_data=(X_val, Y_val), batch_size=256, epochs=500, verbose=2, shuffle=True, callbacks=[checkpointer])
 pd.DataFrame(hist.history).to_csv(path_or_buf='Conv_models/History.csv')
 predictions = model.predict(X_val)
 score = accuracy_score(change(Y_val), change(predictions))
-print('Last epoch\'s score on validation set is ', score)
+print('Last epoch\'s validation score', score)
 df = pd.DataFrame(change(predictions))
 df.to_csv(path_or_buf='Conv_models/Preds_' + str(format(score, '.4f')) + '.csv', index=None, header=None)
-pd.DataFrame(confusion_matrix(change(Y_val), change(predictions))).to_csv(path_or_buf='Conv_models/Result_Conf' + str(format(score, '.4f')) + '__' +  str(i) + '.csv', index=None, header=None)
+pd.DataFrame(confusion_matrix(change(Y_val), change(predictions))).to_csv(path_or_buf='Conv_models/Result_Conf' + str(format(score, '.4f')) + '.csv', index=None, header=None)
+	
+	
+# skf = StratifiedKFold(n_splits=2,shuffle=True)
+# target_train = target_train.reshape(size,)
+
+# for i, (train_index, test_index) in enumerate(skf.split(X, target_train)):
+	# print("TRAIN:", train_index, "TEST:", test_index)
+	# X_train = X[train_index, :]
+	# Y_train = Label_set[train_index, :]
+	# X_val = X[test_index, :]
+	# Y_val = Label_set[test_index, :]
+	# model = None
+	# model = create_model()
+	# train_and_evaluate__model(model, X_train, Y_train, X_val, Y_val, i)
+
+	
+	
+
