da42f1af3ff9ca66c6887b8efdf7de57fe7d7912
Ayushman Pani <126395065+ayushmanpani@users.noreply.github.com>
2024-10-15 21:03:23 +0530

    Update CNN_ECG.py
    improved the performance by updating some new versions of code and implementing libraries more efficiently

--- CNN_ECG.py
+++ CNN_ECG.py
@@ -1,33 +1,26 @@
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 import math
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout, Conv1D, Conv2D, MaxPooling2D, Flatten
 from keras.callbacks import ModelCheckpoint
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
-import pandas as pd
 import scipy.io as sio
 from os import listdir
 from os.path import isfile, join
-import numpy as np
 import keras
 from sklearn.metrics import accuracy_score
 from keras import backend as K
 import sys
 
 
-K.set_image_dim_ordering('tf') #For problems with ordering
+K.set_image_data_format('channels_last')   #For problems with ordering
 
 number_of_classes = 4
 
 def change(x): 
-    answer = np.zeros((np.shape(x)[0]))
-    for i in range(np.shape(x)[0]):
-        max_value = max(x[i, :])
-        max_index = list(x[i, :]).index(max_value)
-        answer[i] = max_index
-    return answer.astype(np.int)
+    return np.argmax(x, axis=1)
 
 if sys.argv[1] == 'cinc':
     #Loading of .mat files from training directory. Only 9000 time steps from every ECG file is loaded
@@ -35,10 +28,12 @@ if sys.argv[1] == 'cinc':
     onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and f[0] == 'A')]
     bats = [f for f in onlyfiles if f[7] == 'm']
     mats = [f for f in bats if (np.shape(sio.loadmat(mypath + f)['val'])[1] >= 9000)] #Choic of only 9k time steps
+    if not mats:
+        raise ValueError("No valid .mat files found with at least 9000 time steps.")
     check = np.shape(sio.loadmat(mypath + mats[0])['val'])[1]
     X = np.zeros((len(mats), check))
-    for i in range(len(mats)):
-        X[i, :] = sio.loadmat(mypath + mats[i])['val'][0,:9000]
+    for i, mat in enumerate(mats):
+        X[i, :] = sio.loadmat(join(mypath, mat))['val'][0, :9000]
 
     #Transformation from literals (Noisy, Arithm, Other, Normal)
     target_train = np.zeros((len(mats), 1))
@@ -53,11 +48,12 @@ if sys.argv[1] == 'cinc':
         else:
             target_train[i] = 3
 
-    Label_set = np.zeros((len(mats), number_of_classes))
+    '''Label_set = np.zeros((len(mats), number_of_classes))
     for i in range(np.shape(target_train)[0]):
         dummy = np.zeros((number_of_classes))
         dummy[int(target_train[i])] = 1
-        Label_set[i, :] = dummy
+        Label_set[i, :] = dummy'''
+    Label_set = np.eye(number_of_classes)[target_train.astype(int)]
         
 elif sys.argv[1] == 'mit':
     print('In proces...')
@@ -131,7 +127,7 @@ model.add(Dense(number_of_classes, activation='softmax'))
 #Callbacks and accuracy calculation
 #early_stopping = keras.callbacks.EarlyStopping(monitor='val_acc', min_delta=0, patience=50, verbose=1, mode='auto')
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-checkpointer = ModelCheckpoint(filepath="Keras_models/weights.{epoch:02d}-{val_acc:.2f}.hdf5", monitor='val_loss', save_weights_only=False, period=1, verbose=1, save_best_only=False)
+checkpointer = ModelCheckpoint(filepath="Keras_models/weights.{epoch:02d}-{val_acc:.2f}.hdf5", monitor='val_loss', save_weights_only=True, period=1, verbose=1, save_best_only=False)
 model.fit(X_train, Y_train, epochs=250, batch_size=batch_size, validation_data=(X_val, Y_val), verbose=2, shuffle=False, callbacks=[checkpointer])
 model.save('Keras_models/my_model_' + str(i) + '_' + str(j) + '_' + str() + '.h5')
 predictions = model.predict(X_val)
